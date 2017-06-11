f52cf27a76f1700c7963a8daee97e3ee23b84688
be5invis <belleve@typeof.net>
2017-06-11 20:46:20 +0800

    Initial commit

---
+++ generate.js
@@ -0,0 +1,318 @@
+const path = require("path");
+const fs = require("fs");
+const color = require("onecolor");
+
+color.RGB.prototype.hexaa = function() {
+	var alphaString = Math.round(this._alpha * 255).toString(16);
+	return "#" + this.hex().substr(1, 6) + "00".substr(0, 2 - alphaString.length) + alphaString;
+};
+
+function invert(c) {
+	return c.lightness(1 - c.lightness()).rgb();
+}
+
+// colors
+const gray = [
+	color("#f8f9fa"),
+	color("#f1f3f5"),
+	color("#e9ecef"),
+	color("#dee2e6"),
+	color("#ced4da"),
+	color("#adb5bd"),
+	color("#868e96"),
+	color("#495057"),
+	color("#343a40"),
+	color("#212529")
+];
+
+const red = [
+	color("#fff5f5"),
+	color("#ffe3e3"),
+	color("#ffc9c9"),
+	color("#ffa8a8"),
+	color("#ff8787"),
+	color("#ff6b6b"),
+	color("#fa5252"),
+	color("#f03e3e"),
+	color("#e03131"),
+	color("#c92a2a")
+];
+
+const green = [
+	color("#ebfbee"),
+	color("#d3f9d8"),
+	color("#b2f2bb"),
+	color("#8ce99a"),
+	color("#69db7c"),
+	color("#51cf66"),
+	color("#40c057"),
+	color("#37b24d"),
+	color("#2f9e44"),
+	color("#2b8a3e")
+];
+
+const blue = [
+	color("#e8f7ff"),
+	color("#ccedff"),
+	color("#a3daff"),
+	color("#72c3fc"),
+	color("#4dadf7"),
+	color("#329af0"),
+	color("#228ae6"),
+	color("#1c7cd6"),
+	color("#1b6ec2"),
+	color("#1862ab")
+];
+
+function generate(gray, red, green, blue) {
+	const bg = gray[1];
+	const fg = gray[7];
+	const stress = blue[5];
+	const border = gray[3];
+	// token settings
+	const keyword = {
+		fontStyle: "bold",
+		foreground: gray[9].hex()
+	};
+	const operator = keyword;
+	const literal = {
+		foreground: gray[9].hex()
+	};
+	const comment = {
+		foreground: gray[5].hex()
+	};
+	const library = {
+		foreground: gray[9].hex()
+	};
+	const quote = {
+		fontStyle: "italic",
+		foreground: gray[6].hex()
+	};
+	const user = {
+		foreground: fg.hex()
+	};
+	const punct = {
+		foreground: gray[6].hex()
+	};
+	const invalid = {
+		foreground: red[7].hex()
+	};
+	const access = {};
+
+	return {
+		$schema: "vscode://schemas/color-theme",
+		name: "Railgun",
+		colors: {
+			focusBorder: stress.hex(),
+			foreground: fg.hex(),
+			errorForeground: red[6].hex(),
+			"editor.background": bg.hex(),
+			"editor.foreground": fg.hex(),
+			"editor.lineHighlightBackground": gray[2].hex(),
+			"editorCursor.foreground": stress.hex(),
+			"editorLineNumber.foreground": gray[5].hex(),
+
+			"editor.selectionBackground": blue[2].hexaa(),
+			"editor.selectionHighlightBackground": blue[1].hexaa(),
+			"editor.inactiveSelectionBackground": gray[3].hexaa(),
+
+			"debugToolBar.background": gray[2].hexaa(),
+			"editorWidget.background": gray[2].hexaa(),
+			"editorSuggestWidget.background": bg.hexaa(),
+
+			"editorGroup.border": border.hex(),
+			"editorGroupHeader.noTabsBackground": gray[1].hex(),
+			"editorGroupHeader.tabsBackground": gray[2].hex(),
+			"tab.border": border.hex(),
+			"tab.inactiveBackground": gray[2].hex(),
+			"tab.inactiveForeground": gray[6].hex(),
+			"tab.activeBackground": gray[1].hex(),
+			"tab.activeForeground": gray[9].hex(),
+
+			"peekViewEditor.background": gray[3].alpha(1 / 3).hexaa(),
+			"peekViewTitle.background": gray[0].hexaa(),
+			"peekView.border": border.hex(),
+
+			"scrollbar.shadow": color("#000000").alpha(0.1).hexaa(),
+			"scrollbarSlider.background": color("#000000").alpha(0.075).hexaa(),
+			"scrollbarSlider.activeBackground": color("#000000").alpha(0.15).hexaa(),
+			"scrollbarSlider.hoverBackground": color("#000000").alpha(0.15).hexaa(),
+
+			"editorOverviewRuler.border": "#00000000",
+			"editorGutter.modifiedBackground": blue[5].hex(),
+			"editorGutter.addedBackground": green[5].hex(),
+			"editorGutter.deletedBackground": red[5].hex(),
+			"diffEditor.removedTextBackground": red[5].alpha(0.15).hexaa(),
+			"diffEditor.insertedTextBackground": green[5].alpha(0.1).hexaa(),
+
+			"sideBarTitle.foreground": gray[9].hex(),
+			"sideBar.background": gray[2].hex(),
+			"sideBar.border": border.hex(),
+			"sideBarSectionHeader.background": gray[3].hex(),
+
+			"list.highlightForeground": blue[6].hex(),
+			"list.hoverBackground": gray[3].hex(),
+			"list.inactiveSelectionBackground": gray[5].alpha(0.3).hexaa(),
+			"list.activeSelectionBackground": stress.alpha(0.25).hexaa(),
+			"list.focusBackground": stress.alpha(0.25).hexaa(),
+			"list.inactiveSelectionForeground": gray[9].hex(),
+			"list.activeSelectionForeground": gray[9].hex(),
+			"list.focusForeground": gray[9].hex(),
+
+			"dropdown.background": gray[0].hex(),
+			"dropdown.border": border.hex(),
+			"dropdown.foreground": gray[7].hex(),
+
+			"input.background": gray[0].hex(),
+			"input.border": border.hex(),
+			"input.foreground": gray[7].hex(),
+			"input.placeholderForeground": gray[5].hex(),
+
+			"button.background": blue[6].hex(),
+			"button.foreground": blue[0].hex(),
+			"button.hoverBackground": blue[7].hex(),
+
+			"badge.background": stress.hex(),
+			"activityBar.background": gray[4].hex(),
+			"activityBar.foreground": gray[8].hex(),
+
+			"statusBar.background": gray[3].hex(),
+			"statusBar.foreground": fg.hex(),
+			"statusBar.noFolderBackground": gray[3].hex(),
+			"statusBar.noFolderForeground": fg.hex(),
+			"statusBar.debuggingBackground": gray[3].hex(),
+			"statusBar.debuggingForeground": fg.hex(),
+
+			"panel.border": border.hex(),
+			"panelTitle.activeBorder": stress.hex()
+		},
+		tokenColors: [
+			{
+				name: "Comment",
+				scope: "comment, punctuation.comment, punctuation.definition.comment",
+				settings: comment
+			},
+			{
+				name: "Operator",
+				scope: "keyword.operator",
+				settings: operator
+			},
+			{
+				name: "Punctuation",
+				scope: "punctuation, delimiter, bracket, brace, paren, delimiter.tag, punctuation.tag, tag.html, tag.xml, meta.property-value punctuation.separator.key-value,punctuation.definition.metadata.md, string.link.md, meta.brace",
+				settings: punct
+			},
+			{
+				name: "String",
+				scope: "string, meta.property-value.string, support.constant.property-value.string, meta.structure.dictionary.value.json string.quoted.double.json, meta.structure.dictionary.json string.quoted.double.json, meta.preprocessor string",
+				settings: quote
+			},
+			{
+				name: "Primitive Literals",
+				scope: "constant.numeric, meta.property-value.numeric, support.constant.property-value.numeric, meta.property-value.color, support.constant.property-value.color,constant.language",
+				settings: literal
+			},
+			{
+				name: "User names",
+				scope: "constant.character, constant.other,entity.name.function, entity.name.class, entity.other.inherited-class, entity.other.attribute-name, entity.name, entity.other.attribute-name, entity.other.attribute-name.html, support.type.property-name, entity.name.tag.table, meta.structure.dictionary.json string.quoted.double.json",
+				settings: user
+			},
+			{
+				name: "Keyword",
+				scope: "keyword, meta.property-value.keyword, support.constant.property-value.keyword, meta.preprocessor.keyword,keyword.other.use, keyword.other.function.use, keyword.other.namespace, keyword.other.new, keyword.other.special-method, keyword.other.unit, keyword.other.use-as",
+				settings: keyword
+			},
+			{
+				name: "Storage",
+				scope: "storage, storage.type, storage.type.ts, storage.type.var.ts, storage.type.js, storage.type.var.js, storage.type.const.ts, storage.type.let.ts, storage.type.let.js, storage.type.const.js, entity.name.tag",
+				settings: keyword
+			},
+			{
+				name: "Pointer, access, etc",
+				scope: "meta.ptr, meta.pointer, meta.address, meta.array.cxx",
+				settings: access
+			},
+			{
+				name: "Preprocessor",
+				scope: "meta.preprocessor",
+				settings: user
+			},
+			{
+				name: "Library",
+				scope: "support.type, support.class, support.function, support.constant",
+				settings: library
+			},
+			{
+				name: "Invalid",
+				scope: "invalid",
+				settings: invalid
+			},
+			{
+				name: "Invalid deprecated",
+				scope: "invalid.deprecated",
+				settings: invalid
+			},
+			{
+				name: "Markdown Title Hash",
+				scope: "punctuation.definition.heading.md, entity.name.type.md, beginning.punctuation",
+				settings: user
+			},
+			{
+				name: "Markdown titles",
+				scope: "markup.heading, entity.name.section",
+				settings: keyword
+			},
+			{
+				name: "Markdown Raw",
+				scope: "markup.raw, markup.inline.raw, markup.fenced, markup.fenced_code",
+				settings: quote
+			},
+			{
+				name: "Markdown link",
+				scope: "markup.link, string.other.link.title, string.other.link.description, meta.link.inline, meta.image.inline",
+				settings: user
+			},
+			{
+				name: "Makefile Variables",
+				scope: "variable.language.makefile, variable.other.makefile",
+				settings: user
+			},
+			{
+				scope: "markup.italic",
+				settings: {
+					fontStyle: "italic"
+				}
+			},
+			{
+				scope: "markup.bold",
+				settings: {
+					fontStyle: "bold"
+				}
+			},
+			{
+				name: "CSS Class",
+				scope: "entity.other.attribute-name.class.css",
+				settings: library
+			},
+			{
+				name: "CSS Tag name",
+				scope: "entity.name.tag.css",
+				settings: keyword
+			},
+			{
+				name: "CSS Property",
+				scope: "meta.property-name.css",
+				settings: user
+			}
+		]
+	};
+}
+
+fs.writeFileSync(
+	path.join(__dirname, "themes", "hildr.json"),
+	JSON.stringify(generate(gray.map(invert), red.map(invert), green.map(invert), blue.map(invert)))
+);
+fs.writeFileSync(
+	path.join(__dirname, "themes", "verdandi.json"),
+	JSON.stringify(generate(gray, red, green, blue))
+);
